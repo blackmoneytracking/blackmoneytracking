2aef239123ddef00b9ba8f4ffe046c6c3872f83d
SousaVictorH <victor.aquino@ccc.ufcg.edu.br>
2020-10-16 11:56:08 -0300

    formatando rotas adicionandas anteriormente

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './style.css';
 
+import logoBranca from '../../icons/logoBranca.png';
+import { Link } from 'react-router-dom';
+
 export default function Contact(){
     return(
-        <div>
-            <h1>Contact</h1>
+        <div className="menu">   
+        <ul>
+        <img src={logoBranca} alt="some text" width='75' height='65' />
+            <Link className="buttonMenu" to = "/">
+                <li><a>Home</a></li>
+            </Link>
 
-            <p>Not yet implemented.</p>
+            <Link className="buttonMenu" to = "/onlineStores">
+                <li><a>Lojas virtuais</a></li>
+            </Link>
+
+            <Link className="buttonMenu" to = "/ongs">
+                <li><a href="#">Ongs</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/register">
+                <li><a>Cadastrar loja</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/about">
+                <li><a>Sobre</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/contact">
+                <li><a>Contato</a></li>
+            </Link>
+        </ul>
+
+        <div className="contact">
+
+            <h1>Contact:</h1>
+            <p> 
+                Not yet implemented!
+            </p>
         </div>
+    </div>
     );
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './style.css';
 
+import logoBranca from '../../icons/logoBranca.png';
+import { Link } from 'react-router-dom';
+
 export default function Register(){
     return(
-        <div>
-            <h1>Register</h1>
+        <div className="menu">   
+        <ul>
+        <img src={logoBranca} alt="some text" width='75' height='65' />
+            <Link className="buttonMenu" to = "/">
+                <li><a>Home</a></li>
+            </Link>
 
-            <p>Not yet implemented.</p>
+            <Link className="buttonMenu" to = "/onlineStores">
+                <li><a>Lojas virtuais</a></li>
+            </Link>
+
+            <Link className="buttonMenu" to = "/ongs">
+                <li><a href="#">Ongs</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/register">
+                <li><a>Cadastrar loja</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/about">
+                <li><a>Sobre</a></li>
+            </Link>
+            <Link className="buttonMenu" to = "/contact">
+                <li><a>Contato</a></li>
+            </Link>
+        </ul>
+
+        <div className="register">
+
+            <h1>Register:</h1>
+            <p> 
+                Not yet implemented!
+            </p>
         </div>
+    </div>
     );
 }
